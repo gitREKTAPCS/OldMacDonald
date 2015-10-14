0c87b6c5747bcb1b0de493940e0fd0d809109f4a
j1lin0101 <j1lin0101@gmail.com>
2015-10-14 14:59:50 -0700

    added name cow

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,17 @@
-class NamedCow
+class NamedCow extends Cow
 {
+	private String myType;     
+    private String mySound;
+	private String name;
+	 public NamedCow(String type,String cowName, String sound)     {         
+         myType = type;  
+         name = cowName;       
+         mySound = sound;     
+     }     
+     public NamedCow()     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }      
 
+	public String getName(){return name;}
 }
