c03d3196055adc9a6ce97cd31d60f9c9122a588f
j1lin0101 <j1lin0101@gmail.com>
2015-10-14 15:17:52 -0700

    no more unknown

--- NamedCow.java
+++ NamedCow.java
@@ -1,16 +1,12 @@
 class NamedCow extends Cow
 {
-	private String myType;     
-    private String mySound;
 	private String name;
 	 public NamedCow(String type,String cowName, String sound)     {         
          myType = type;  
          name = cowName;       
          mySound = sound;     
      }     
-     public NamedCow()     {         
-         myType = "unknown";         
-         mySound = "unknown";     
+     public NamedCow()     { 
      }      
 
 	public String getName(){return name;}
